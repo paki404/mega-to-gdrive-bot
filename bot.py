e78d2993f4cc2551b93a9a1eb93baf1746b4a68f
paki404 <pakistanikira4@gmail.com>
2026-06-27 14:47:34 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,11 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message, CallbackQuery
-from plugins import *
+from plugins.login import mega_login
+from plugins.get import mega_get
+from plugins.ls import mega_ls
+from plugins.rm import mega_rm
+from plugins.importt import mega_import
+from plugins.gdrive import gdrive_mega1, gdrive_answer
 from userbot import app
 from os import environ
 
@@ -8,29 +13,26 @@ my_user_id = environ["TELEGRAM_USER_ID"]
 
 @app.on_message(filters.command(["start"]))
 def start(client: Client, message: Message):
-    # message_id -> id kar diya aur reply_text use kiya
-    message.reply_text("Welcome")
-
-@app.on_message(filters.regex(r'#other'))
-def megaother(client: Client, message: Message):
-    mega_other(client, message)
+    message.reply_text("**Welcome to Multi-User Mega Bot**\n\n"
+                       "1. `/login email password`\n"
+                       "2. `/ls`, `/get`, `/rm`, `/import`")
 
 @app.on_message(filters.regex(r'#login'))
 def megalogin(client: Client, message: Message):
     mega_login(client, message)
 
 @app.on_message(filters.regex(r'#rm'))
-def mega_rm(client: Client, message: Message):
-    mega_rm_file(client, message)
+def mega_rm_cmd(client: Client, message: Message):
+    mega_rm(client, message)
 
 @app.on_message(filters.regex(r'#import'))
-def mega_import(client: Client, message: Message):
-    mega_import_file(client, message)
+def mega_import_cmd(client: Client, message: Message):
+    mega_import(client, message)
 
 @app.on_message(filters.regex(r'#get|mega.co.nz|mega.nz'))
-def mega_get(client: Client, message: Message):
-    mega_get_link_file(client, message)
-        
+def mega_get_cmd(client: Client, message: Message):
+    mega_get(client, message)
+
 @app.on_message(filters.regex(r'#ls'))
 def megals(client: Client, message: Message):
     mega_ls(client, message)
@@ -41,8 +43,7 @@ def gdrive_mega(client: Client, message: Message):
 
 # Ye function bahar nikala hai
 @app.on_callback_query()
-def gdrive_megaa(client: Client, callback_query: CallbackQuery):
+def gdrive_mega_callback(client: Client, callback_query: CallbackQuery):
     gdrive_answer(client, callback_query)
-    mega_answer(client, callback_query)
 
 app.run()
