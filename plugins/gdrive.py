b089555b1422d1c4065b667eaff94c770dc437dc
paki404 <pakistanikira4@gmail.com>
2026-06-27 14:29:47 +0500

    Update gdrive.py

--- plugins/gdrive.py
+++ plugins/gdrive.py
@@ -1,196 +1,129 @@
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
-from pydrive.drive import GoogleDrive
-from pyrogram import Client, filters
+from pyrogram import Client, filters, enums
 from pydrive.auth import GoogleAuth
-from datetime import datetime
-from userbot import app
-import subprocess
-import traceback
-import shutil
-import pytz
+from pydrive.drive import GoogleDrive
+from mega import Mega
+import os
+import humanize
+import time
+import asyncio
+import sqlite3
+from threading import Thread
 
-def gdrive_mega1(client: Client, message: Message):
-    global coming_user_id
-    coming_user_id = message.from_user.id
-    
-    global reply_to_message_ids
-    reply_to_message_ids = message.message_id
+DB_NAME = "users.db"
 
-    #getting file name
-    global file_name
-    file_name = None
-    if message.media:
-        if message.audio:
-            file_name = message.audio.file_name
-        elif message.document:
-            file_name = message.document.file_name
-        elif message.video:
-            file_name = message.video.file_name
-
-    what_msg = app.send_message(coming_user_id,f"Where you want to upload {file_name}?",reply_to_message_id=message.message_id,reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("gdrive",callback_data="gdrive"),InlineKeyboardButton("mega",callback_data="mega")]]))
-    
-@app.on_callback_query(filters.regex(r'gdrive'))
-def gdrive_answer(client: Client, callback_query: CallbackQuery):
-    #getting file id
-    file_id = None
-    if callback_query.message.reply_to_message.media:
-        if callback_query.message.reply_to_message.audio:
-            file_id = callback_query.message.reply_to_message.audio.file_id
-        elif callback_query.message.reply_to_message.document:
-            file_id = callback_query.message.reply_to_message.document.file_id
-        elif callback_query.message.reply_to_message.photo:
-            file_id = callback_query.message.reply_to_message.photo.file_id
-        elif callback_query.message.reply_to_message.video:
-            file_id = callback_query.message.reply_to_message.video.file_id
-    msg = app.send_message(chat_id=coming_user_id, reply_to_message_id=reply_to_message_ids, text="Working on it.",parse_mode="html")
+def get_mega(user_id):
+    """Database se user ka mega login nikalta hai"""
     try:
-        call_data = file_id
-        print(call_data)
-        aa = callback_query.message.message_id
-        callback_query.message.delete()
-        if file_name is None:
-            #login        
-            gauth = GoogleAuth()
-            gauth.LoadCredentialsFile("mycreds.txt")
-            drive = GoogleDrive(gauth)
-
-            #downloading
-            IST = pytz.timezone('Asia/Kolkata')
-            download_starttime = datetime.now(IST)
-            downloading_msg = app.send_message(chat_id=coming_user_id, reply_to_message_id=reply_to_message_ids, text="Downloading image please wait.",parse_mode="html")
-            download_file = app.download_media(call_data)
-            print(download_file)
-            time_taken_for_download = (datetime.now(IST) - download_starttime).seconds
-            print('Downloaded in',time_taken_for_download,'seconds')
-
-            #uploading
-            uploading_msg = app.send_message(chat_id=coming_user_id, reply_to_message_id=reply_to_message_ids, text="Uploading image to Gdrive please wait.",parse_mode="html")
-            orig = download_file
-            upload_starttime = datetime.now(IST)
-            file1 = drive.CreateFile()
-            file1.SetContentFile(orig)
-            file1.Upload()
-            link=file1['alternateLink']
-            time_taken_for_upload = (datetime.now(IST) - upload_starttime).seconds
-            print('Uploaded in',time_taken_for_upload,'seconds')
-        
-            #sending url
-            downloading_msg.delete()
-            uploading_msg.delete()
-            app.send_message(chat_id=coming_user_id, reply_to_message_id=reply_to_message_ids, text=link)
-            app.send_message(chat_id=coming_user_id, reply_to_message_id=reply_to_message_ids, text="Downloaded in " + str(time_taken_for_download) + " seconds. \nUploaded in " + str(time_taken_for_upload) + " seconds.")
-            print(link)
-
-        elif file_name is not None:
-            #login        
-            gauth = GoogleAuth()
-            gauth.LoadCredentialsFile("mycreds.txt")
-            drive = GoogleDrive(gauth)
-
-            #downloading
-            IST = pytz.timezone('Asia/Kolkata')
-            download_starttime = datetime.now(IST)
-            downloading_msg = app.send_message(chat_id=coming_user_id, reply_to_message_id=reply_to_message_ids, text="Downloading " + '<b>' + file_name + '</b>' + " please wait.",parse_mode="html")
-            download_file = app.download_media(call_data)
-            print(download_file)
-            time_taken_for_download = (datetime.now(IST) - download_starttime).seconds
-            print('Downloaded in',time_taken_for_download,'seconds')
-
-            #uploading
-            uploading_msg = app.send_message(chat_id=coming_user_id, reply_to_message_id=reply_to_message_ids, text="Uploading " + '<b>' + file_name + '</b>' + " to Gdrive please wait.",parse_mode="html")
-            orig = shutil.copy(download_file, file_name)
-            upload_starttime = datetime.now(IST)
-            file1 = drive.CreateFile()
-            file1.SetContentFile(orig)
-            file1.Upload()
-            link=file1['alternateLink']
-            time_taken_for_upload = (datetime.now(IST) - upload_starttime).seconds
-            print('Uploaded in',time_taken_for_upload,'seconds')
-        
-            #sending url
-            downloading_msg.delete()
-            uploading_msg.delete()
-            app.send_message(chat_id=coming_user_id, reply_to_message_id=reply_to_message_ids, text=link)
-            app.send_message(chat_id=coming_user_id, reply_to_message_id=reply_to_message_ids, text="Downloaded in " + str(time_taken_for_download) + " seconds. \nUploaded in " + str(time_taken_for_upload) + " seconds.")
-            print(link)
-            
-        msg.delete()
+        conn = sqlite3.connect(DB_NAME)
+        c = conn.cursor()
+        c.execute("SELECT mega_email, mega_pass FROM users WHERE user_id =?", (user_id,))
+        res = c.fetchone()
+        conn.close()
+        return res # (email, pass) ya None
     except:
-        error_var = traceback.format_exc()
-        msg.delete()
-        app.send_message(chat_id=coming_user_id, text="Got error.",parse_mode="html")
-        app.send_message(chat_id=coming_user_id, text=error_var,parse_mode="html")
+        return None
 
-@app.on_callback_query(filters.regex(r'mega')) 
-def mega_answer(client: Client, callback_query: CallbackQuery):
-    #getting file id
-    file_id = None
-    if callback_query.message.reply_to_message.media:
-        if callback_query.message.reply_to_message.audio:
-            file_id = callback_query.message.reply_to_message.audio.file_id
-        elif callback_query.message.reply_to_message.document:
-            file_id = callback_query.message.reply_to_message.document.file_id
-        elif callback_query.message.reply_to_message.photo:
-            file_id = callback_query.message.reply_to_message.photo.file_id
-        elif callback_query.message.reply_to_message.video:
-            file_id = callback_query.message.reply_to_message.video.file_id
-    msg = app.send_message(chat_id=coming_user_id, reply_to_message_id=reply_to_message_ids, text="Working on it.",parse_mode="html")
+def gdrive_mega1(app, message): # <-- bot.py isko call karta hai
+    """Telegram file -> User ka apna Mega upload"""
+    user_creds = get_mega(message.from_user.id)
+    if not user_creds:
+        return message.reply_text("❌ Pehle `/login email password` se Mega account add karo.")
+
+    MEGA_EMAIL, MEGA_PASS = user_creds
+
+    if not (message.document or message.video or message.audio or message.photo):
+        return message.reply_text("📁 File bhejo.")
+
     try:
-        call_data = file_id
-        print(call_data)
-        aa = callback_query.message.message_id
-        callback_query.message.delete()   
-        if file_name is None:
-            #downloading
-            IST = pytz.timezone('Asia/Kolkata')
-            download_starttime = datetime.now(IST)
-            downloading_msg = app.send_message(coming_user_id, reply_to_message_id=reply_to_message_ids, text="Downloading image please wait.",parse_mode="html")
-            download_file = app.download_media(call_data)
-            print(download_file)
-            time_taken_for_download = (datetime.now(IST) - download_starttime).seconds
-            print('Downloaded in',time_taken_for_download,'seconds')
+        msg = message.reply_text("`Downloading... 0%`")
+        c_time = time.time()
 
-            #uploading
-            uploading_msg = app.send_message(coming_user_id, reply_to_message_id=reply_to_message_ids, text="Uploading image to mega please wait.",parse_mode="html")
-            orig = download_file
-            upload_starttime = datetime.now(IST)
-            upload_file = subprocess.check_output('mega-put --ignore-quota-warn ' + orig,shell=True)
-            import_msg_decode = upload_file.decode()
-            time_taken_for_upload = (datetime.now(IST) - upload_starttime).seconds
-            print('Uploaded in',time_taken_for_upload,'seconds')
-    
-            #sending message
-            downloading_msg.delete()
-            uploading_msg.delete()
-            app.send_message(coming_user_id, reply_to_message_id=reply_to_message_ids, text=str(import_msg_decode) + "\n" + "Downloaded in " + str(time_taken_for_download) + " seconds. \nUploaded in " + str(time_taken_for_upload) + " seconds.")
+        file_path = app.download_media(
+            message,
+            progress=progress_for_pyrogram,
+            progress_args=("📥 Downloading", msg, c_time)
+        )
 
-        elif file_name is not None:
-            #downloading
-            IST = pytz.timezone('Asia/Kolkata')
-            download_starttime = datetime.now(IST)
-            downloading_msg = app.send_message(coming_user_id, reply_to_message_id=reply_to_message_ids, text="Downloading " + '<b>' + file_name + '</b>' + " please wait.",parse_mode="html")
-            download_file = app.download_media(call_data)
-            print(download_file)
-            time_taken_for_download = (datetime.now(IST) - download_starttime).seconds
-            print('Downloaded in',time_taken_for_download,'seconds')
+        file_size = os.path.getsize(file_path)
+        file_name = os.path.basename(file_path)
+        msg.edit_text(f"✅ Downloaded\n**Size:** {humanize.naturalsize(file_size)}\n\n`Logging into your Mega...`")
 
-            #uploading
-            uploading_msg = app.send_message(coming_user_id, reply_to_message_id=reply_to_message_ids, text="Uploading " + '<b>' + file_name + '</b>' + " to mega please wait.",parse_mode="html")
-            orig = shutil.copy(download_file, file_name)
-            upload_starttime = datetime.now(IST)
-            upload_file = subprocess.check_output('mega-put --ignore-quota-warn ' + orig,shell=True)
-            import_msg_decode = upload_file.decode()
-            time_taken_for_upload = (datetime.now(IST) - upload_starttime).seconds
-            print('Uploaded in',time_taken_for_upload,'seconds')
-    
-            #sending message
-            downloading_msg.delete()
-            uploading_msg.delete()
-            app.send_message(coming_user_id, reply_to_message_id=reply_to_message_ids, text=str(import_msg_decode) + "\n" + "Downloaded in " + str(time_taken_for_download) + " seconds. \nUploaded in " + str(time_taken_for_upload) + " seconds.")
- 
-        msg.delete()
-    except:
-        error_var = traceback.format_exc()
-        msg.delete()
-        app.send_message(chat_id=coming_user_id, reply_to_message_id=reply_to_message_ids, text="Got error.",parse_mode="html")
-        app.send_message(chat_id=coming_user_id, reply_to_message_id=reply_to_message_ids, text=error_var,parse_mode="html")
+        mega = Mega()
+        m = mega.login(MEGA_EMAIL, MEGA_PASS)
+
+        msg.edit_text("`Uploading to Mega.nz... 0%`")
+
+        def upload_task():
+            m.upload(file_path, None, callback=lambda x, y: progress_mega(x, y, msg))
+            os.remove(file_path)
+            msg.edit_text(f"✅ Uploaded to your Mega.nz\n**File:** `{file_name}`")
+
+        Thread(target=upload_task, daemon=True).start()
+
+    except Exception as e:
+        message.reply_text(f"**Mega Error:** `{e}`\nPassword galat ho sakta hai.")
+
+def gdrive_answer(app, message):
+    """Telegram file -> Google Drive upload"""
+    if not (message.document or message.video or message.audio or message.photo):
+        return message.reply_text("📁 File bhejo.")
+
+    try:
+        msg = message.reply_text("`Authenticating Google Drive...`")
+        gauth = GoogleAuth()
+        gauth.LoadCredentialsFile("token.txt")
+        if gauth.credentials is None:
+            gauth.LocalWebserverAuth()
+        elif gauth.access_token_expired:
+            gauth.Refresh()
+        else:
+            gauth.Authorize()
+        gauth.SaveCredentialsFile("token.txt")
+        drive = GoogleDrive(gauth)
+
+        msg.edit_text("`Downloading...`")
+        c_time = time.time()
+        file_path = app.download_media(
+            message,
+            progress=progress_for_pyrogram,
+            progress_args=("📥 Downloading", msg, c_time)
+        )
+
+        file_name = os.path.basename(file_path)
+        msg.edit_text("`Uploading to Google Drive...`")
+        gfile = drive.CreateFile({'title': file_name})
+        gfile.SetContentFile(file_path)
+        gfile.Upload()
+        os.remove(file_path)
+
+        msg.edit_text(f"✅ Uploaded\n**Link:** https://drive.google.com/file/d/{gfile['id']}/view")
+
+    except Exception as e:
+        message.reply_text(f"**GDrive Error:** `{e}`")
+
+async def progress_for_pyrogram(current, total, ud_type, message, start):
+    now = time.time()
+    diff = now - start
+    if round(diff % 5.00) == 0 or current == total:
+        try:
+            percentage = current * 100 / total
+            speed = current / diff if diff > 0 else 0
+            time_to_completion = round((total - current) / speed) if speed > 0 else 0
+            eta = time.strftime("%H:%M:%S", time.gmtime(time_to_completion))
+            progress = "[{0}{1}] {2}%\n".format(
+                ''.join(["█" for i in range(int(percentage / 5))]),
+                ''.join(["░" for i in range(20 - int(percentage / 5))]),
+                round(percentage, 2))
+            tmp = progress + f"`{humanize.naturalsize(current)}` of `{humanize.naturalsize(total)}`\n**Speed:** `{humanize.naturalsize(speed)}/s`\n**ETA:** `{eta}`"
+            await message.edit_text(f"{ud_type}\n\n{tmp}")
+        except:
+            pass
+
+def progress_mega(uploaded, total, msg):
+    try:
+        if total > 0:
+            percent = (uploaded / total) * 100
+            text = f"`Uploading to Mega.nz... {round(percent, 2)}%`"
+            asyncio.run_coroutine_threadsafe(msg.edit_text(text), msg._client.loop)
+    except Exception:
+        pass
